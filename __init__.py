8a805ca430f943e227a34b71c45d57f42da100e2
QiuShanLing <47919709+QiuShanLing@users.noreply.github.com>
2026-01-11 00:40:06 +0800

    更新环境

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from src.ffe_parse import FFEParser
+import src
 
 __version__ = '0.1.0'
-__all__ = ['FFEParser']
+__all__ = ['FFEParser', 'src']
 __author__ = 'QiuShanLing'
 __email__ = 'wzwymail@163.com'
